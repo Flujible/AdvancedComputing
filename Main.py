d3123865cedb4951047d4bc8d348830092846752
George Bryant <george.s.bryant@gmail.com>
2017-02-07 18:39:02 +0000

    Add user code for a reducer

--- Main.py
+++ Main.py
@@ -3,6 +3,15 @@
 
 def mapper(inputLine, output):
     inputLine = inputLine.split(",")
-    for word in input:
+    for word in inputLine:
         output.key = word
         output.value = 1
+
+
+# I dont understand how this is supposed to work, it somewhat mimics the wordcount hadoop example
+def reducer(kVPairs, output):
+    cnt = 0
+    for pair in kVPairs:
+        cnt+= pair.value
+    output.setKey(kVPairs.key)
+    output.setValue(cnt)
