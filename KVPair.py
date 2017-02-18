4b430b447b85fc1af758861bd22d1dea25e5e3dc
George Bryant <george.s.bryant@gmail.com>
2017-02-18 14:06:51 +0000

    Added steps towards reducing the mapper output, still WIP

--- KVPair.py
+++ KVPair.py
@@ -1,6 +1,7 @@
 """Defines the KVpair (Key/Value Pair) object that will be used for mapReduce processing"""
 
-class KeyValuePair:
+class KVPair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
+        self.reduced = False
--- Reducer.py
+++ Reducer.py
@@ -2,14 +2,20 @@
 
 class Reducer:
     def __init__(self):
-        pass
+        self.kvPairs = []
 
-    def reducer(self, kvPairs):
+    def reducer(self, kvPair):
         print(":: Default reducer - custom reducer not configured")
 
     def run(self):
-        #Split the file into lines, and then execute the mapper function on each line
-        kvPairs = []
-        for line in lines:
-            kvPairs.append(self.mapper(line))
-        return kvPairs
+        reducedPairs = []
+        for pairs in self.kvPairs:
+            keyMatch = []
+            for comPairs in self.kvPairs:
+                if pairs.key == comPairs.key and comPairs.reduced == False:
+                    keyMatch.append(comPairs)
+                    comPairs.reduced = True
+            reducedPairs.append(self.reducer(keyMatch))
+        result = open("result.txt", 'w')
+        for pair in reducedPairs:
+            result.write(str(pair.key + ": " + pair.value))
