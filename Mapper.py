5e49909fabe7bb0176ba1fd0cb78f6e8c05f8adb
George Bryant <george.s.bryant@gmail.com>
2017-02-18 14:08:19 +0000

    Finished the mapper framework which works for word count

--- Mapper.py
+++ Mapper.py
@@ -8,9 +8,19 @@ class Mapper:
         print(":: Default mapper - custom mapper not configured")
 
     def run(self, inputFile):
-        #Split the file into lines, and then execute the mapper function on each line
-        lines = inputFile.readLine()
-        kvPairs = []
-        for line in lines:
-            kvPairs.append(self.mapper(line))
-        return kvPairs
+        #Split the file into lines, and then execute the mapper function on each line#
+        # lines = inputFile.read().split("\n")
+        return [ self.mapper(line) for line in inputFile.read().split("\n") if line ]
+        # inputFile = inputFile.read()
+        # print(":: InputFile: " + inputFile)
+        # lines = inputFile.split("\n")
+        # for line in lines:
+        #     if line == "":
+        #         print(":: line removed")
+        #         lines.remove(line)
+        # print(":: No. of lines: " + str(len(lines)))
+        # kvPairs = []
+        # for line in lines:
+        #     pair = self.mapper(line)
+        #     kvPairs.append(pair)
+        # return kvPairs
