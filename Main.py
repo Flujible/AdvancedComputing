3e0211f5af1288a9dbfd4f500b9c53d9321d4d34
George Bryant <george.s.bryant@gmail.com>
2017-02-18 14:07:32 +0000

    Fully implement mapper for word count and shorten input text

--- Main.py
+++ Main.py
@@ -1,18 +1,54 @@
 """Defines the tasks for the mapper and the reducer to perform on the data file"""
-import KeyValuePair
+from KVPair import KVPair
+from Mapper import Mapper
+from Reducer import Reducer
+import string
 
-def mapper(inputLine):
-    inputLine = inputLine.split(" ")
-    kvPairs = []
-    for word in inputLine:
-        pair = KeyValuePair(word.word, 1)
-        kvPairs.append(pair)
-    return kvPairs
+class MyMapper(Mapper):
+    """Class to override the original mapper class functions"""
+    def __init__(self):
+        Mapper.__init__(self)
 
-# The reducer itterates over KVPairs where the key is teh same, then moves onto the next set of keys
-def reducer(kVPairs, output):
-    cnt = 0
-    for pair in kVPairs:
-        cnt += pair.value
-    output.setKey(kVPairs.key)
-    output.setValue(cnt)
+    # The mapper class takes a file and performs the below function on each line of that file
+    def mapper(self, inputLine):
+        inputLine = inputLine.split(" ")
+        kvPairs = []
+        for word in inputLine:
+            if "." in word or "," in word:
+                newWord = word.strip(string.punctuation)
+                inputLine.remove(word)
+                inputLine.append(newWord)
+            if word == "":
+                inputLine.remove(word)
+        for word in inputLine:
+            pair = KVPair(word, 1)
+            kvPairs.append(pair)
+        return kvPairs
+
+class MyReducer(Reducer):
+    """Class to override the original reducer class functions"""
+    def __init__(self):
+        Reducer.__init__(self)
+
+    # The reducer itterates over KVPairs where the key is teh same, then moves onto the next set of keys
+    def reducer(self, kVPairs):
+        cnt = 0
+        for pair in kVPairs:
+            cnt += pair.value
+
+        output = KVPair(kVPairs.key, cnt)
+        return output
+
+myMapper = MyMapper()
+myReducer = MyReducer()
+
+inputFile = open("./inputFiles/Sample.txt")
+if inputFile:
+    print(":: File opened successfully")
+else:
+    print(":: Could not open file")
+
+results = myMapper.run(inputFile)
+
+for result in results[0]:
+    print(result.key + ": " + str(result.value))
