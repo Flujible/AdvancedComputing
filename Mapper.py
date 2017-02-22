d23d056eb40469a4e6213761532b780f4aaa8eb9
George Bryant <george.s.bryant@gmail.com>
2017-02-22 11:19:19 +0000

    Change the way the mapper handles a single line of input vs multiple lines of input

--- Mapper.py
+++ Mapper.py
@@ -11,22 +11,14 @@ class Mapper:
         # Split the file into lines, and then execute the mapper function on each line#
         # lines = inputFile.read().split("\n")
         mapped = [ self.mapper(line) for line in inputFile.read().lower().split("\n") if line ]
-        for line in mapped:
-            if line == mapped[0]:
-                continue
-            mapped[0] = mapped[0] + line
+        print(":: Found " + str(len(mapped)) + " lines")
+        if len(mapped) > 1:
+            for line in mapped:
+                if line == mapped[0]:
+                    continue
+                mapped[0] = mapped[0] + line
+                result = mapped[0]
+        else:
             result = mapped[0]
+        print(":: Found " + str(len(result)) + " words")
         return result
-        # inputFile = inputFile.read()
-        # print(":: InputFile: " + inputFile)
-        # lines = inputFile.split("\n")
-        # for line in lines:
-        #     if line == "":
-        #         print(":: line removed")
-        #         lines.remove(line)
-        # print(":: No. of lines: " + str(len(lines)))
-        # kvPairs = []
-        # for line in lines:
-        #     pair = self.mapper(line)
-        #     kvPairs.append(pair)
-        # return kvPairs
