ce6ec224f0afa9b2ecd6b47560bb3d9762539ad8
George Bryant <george.s.bryant@gmail.com>
2017-02-22 11:19:41 +0000

    Write results to file

--- Reducer.py
+++ Reducer.py
@@ -8,29 +8,25 @@ class Reducer:
         print(":: Default reducer - custom reducer not configured")
 
     def run(self, kvPairs):
-        print("::" + str(len(kvPairs)))
-        # reducedPairs = []
+        reducedPairs = []
         # Take each k/v pair and build a list of k/v pairs with the same key
         for pairs in kvPairs:
             matchingPairs = []
             if(not pairs.reduced):
-                # print("\n:: " + pairs.key + " found")
                 matchingPairs.append(pairs)
                 pairs.reduced = True
             else:
-                # print("\n:: " + pairs.key + " found but already counted")
                 continue
             for comPairs in kvPairs:
-                # print(":: Comparing " + pairs.key + " with " + comPairs.key)
                 if pairs.key == comPairs.key and comPairs.reduced == False:
-                    # print(":: " + comPairs.key + " is a match")
                     matchingPairs.append(comPairs)
                     comPairs.reduced = True
                 elif pairs.key == comPairs.key and comPairs.reduced == True:
                     continue
-                    # print(":: " + comPairs.key + " is a match, but is already being considered")
             print(":: '" + pairs.key + "' found " + str(len(matchingPairs)) + " times")
-            # reducedPairs.append(self.reducer(keyMatch))
-        # result = open("result.txt", 'w')
-        # for pair in reducedPairs:
-        #     result.write(str(pair.key + ": " + pair.value))
+            reducedPairs.append(self.reducer(matchingPairs))
+        result = open("result.json", 'w')
+        result.write("{\n")
+        for pair in reducedPairs:
+            result.write('    "' + str(pair.key) + '": ' + str(pair.value) + ",\n")
+        result.write("}")
