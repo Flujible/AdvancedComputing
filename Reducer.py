566faf5638c4b299578000bbf02ee45d6de3ae13
George Bryant <george.s.bryant@gmail.com>
2017-02-23 19:07:26 +0000

    WIP - Add a reducer that takes two arrays of KV Pairs and operates on them

--- Reducer.py
+++ Reducer.py
@@ -24,16 +24,49 @@ class Reducer:
             else:
                 continue
             for comPairs in kvPairs:
-                if pairs.key == comPairs.key and comPairs.reduced == False:
+                if pairs.key == comPairs.key and not comPairs.reduced:
                     matchingPairs.append(comPairs)
                     comPairs.reduced = True
                 elif pairs.key == comPairs.key and comPairs.reduced == True:
                     continue
-            # print(":: '" + pairs.key + "' found " + str(len(matchingPairs)) + " times")
             reducedPairs.append(self.reducer(self, matchingPairs))
-        #result = open("result.json", 'w')
-        # result.write("{\n")
-        # for pair in reducedPairs[:-1]:
-        #     result.write('    "' + str(pair.key) + '": ' + str(pair.value) + ",\n")
-        # result.write('    "' + str(pair.key) + '": ' + str(pair.value) + "\n")
-        # result.write("}")
+        print(reducedPairs)
+        result = open(self.outputFile, 'w')
+        for pair in reducedPairs:
+            result.write(str(pair.key) + ", " + str(pair.value) + "\n")
+        result.close()
+
+    def runMultiple(self, kvPairs1, kvPairs2):
+        reducedPairs = []
+        # Take each k/v pair and build a list of k/v pairs with the same key
+        matchingPairs = []
+        for pairs in kvPairs1
+            if(not pairs.reduced):
+                matchingPairs.append(pairs)
+                pairs.reduced = True
+            else:
+                continue
+            for comPairs in kvPairs1:
+                if pairs.key == comPairs.key and not comPairs.reduced:
+                    matchingPairs.append(comPairs)
+                    comPairs.reduced = True
+                elif pairs.key == comPairs.key and comPairs.reduced == True:
+                    continue
+        #This also needs to compare with what is already in matchinPairs
+        for pairs in kvPairs2:
+            if(not pairs.reduced):
+                matchingPairs.append(pairs)
+                pairs.reduced = True
+            else:
+                continue
+            for comPairs in kvPairs2:
+                if pairs.key == comPairs.key and not comPairs.reduced:
+                    matchingPairs.append(comPairs)
+                    comPairs.reduced = True
+                elif pairs.key == comPairs.key and comPairs.reduced == True:
+                    continue
+            reducedPairs.append(self.reducer(self, matchingPairs))
+        result = open(self.outputFile, 'w')
+        for pair in reducedPairs:
+            result.write(str(pair.key) + ", " + str(pair.value) + "\n")
+        result.close()
