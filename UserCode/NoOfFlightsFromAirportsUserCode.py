a0e5f37b98da58e904aa337a114e11d6ed1d1645
George Bryant <george.s.bryant@gmail.com>
2017-02-24 17:23:33 +0000

    Removed unneeded comments

--- UserCode/NoOfFlightsFromAirportsUserCode.py
+++ UserCode/NoOfFlightsFromAirportsUserCode.py
@@ -1,39 +1,8 @@
+"""File to contain the functions needed to execute the task:  Determine the number
+ of flights from each airport, include a list of any airports not used."""
+
 from KVPair import KVPair
 import re
-# AComp_Passenger_data column headers:
-    # column[0] = Passenger ID
-    # column[1] = Flight ID
-    # column[2] = Dept. airport code
-    # column[3] = Arr. airport code
-    # column[4] = Departure time GMT (Unix epoch time)
-    # column[5] = Total Flight time (mins)
-
-
-# Mapper for word count
-    # newInput = []
-    # kvPairs = []
-    # for word in inputLine:
-        # if word == "":
-        #     continue
-        # elif "." in word or "," in word:
-        #     newWord = word.strip(string.punctuation)
-        #     newInput.append(newWord)
-        # else:
-        #     newInput.append(word)
-    # for word in newInput:
-    #     pair = KVPair(word, 1)
-    #     kvPairs.append(pair)
-    # return kvPairs
-
-# Reducer for word count
-    # cnt = 0
-    # for pair in kvPairs:
-    #     cnt += pair.value
-    # output = KVPair(kvPairs[0].key, cnt)
-    # return output
-
-"""Class to contain the functions needed to execute the task:  Determine the number
- of flights from each airport, include a list of any airports not used."""
 
 def mapUnusedAirports(self, inputLine):
     inputLine = inputLine.split(",")
