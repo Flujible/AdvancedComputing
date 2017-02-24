547599d94f23c7d3ae5bb8e1acb9bf6f00ec9229
George Bryant <george.s.bryant@gmail.com>
2017-02-24 17:23:54 +0000

    Add runner command for another task

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
 """File to control each mapReduce task for the coursework"""
 from Controllers import NoOfFlightsFromAirports
+from Controllers import PassengersOnEachFlight
 
 # Determine the number of flights from each airport, including those with no flights
-NoOfFlightsFromAirports.start()
+# NoOfFlightsFromAirports.start()
+
+# Calculate the number of passengers on each flight
+PassengersOnEachFlight.start()
