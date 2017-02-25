c28b119e89d56d5c909c28cd8736534947c92df4
George Bryant <george.s.bryant@gmail.com>
2017-02-25 17:20:24 +0000

    Added runner for the next task

--- Main.py
+++ Main.py
@@ -1,9 +1,15 @@
 """File to control each mapReduce task for the coursework"""
 from Controllers import NoOfFlightsFromAirports
 from Controllers import PassengersOnEachFlight
+from Controllers import FlightInformation
 
 # Determine the number of flights from each airport, including those with no flights
-# NoOfFlightsFromAirports.start()
+NoOfFlightsFromAirports.start()
 
 # Calculate the number of passengers on each flight
 PassengersOnEachFlight.start()
+
+# Create a list of flights based on the Flight id, this output should include the
+# passenger Id, relevant IATA/FAA codes, the departure time, the arrival time
+# (times to be converted to HH:MM:SS format), and the flight times.
+FlightInformation.start()
