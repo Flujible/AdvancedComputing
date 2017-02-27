fc6d08c7a1ad2f66256699d4de2d9423c60cc091
George Bryant <george.s.bryant@gmail.com>
2017-02-27 20:18:52 +0000

    Add Error stripping as first map reduce job

--- Main.py
+++ Main.py
@@ -1,7 +1,12 @@
 """File to control each mapReduce task for the coursework"""
+from Controllers import StripErrors
 from Controllers import NoOfFlightsFromAirports
 from Controllers import PassengersOnEachFlight
 from Controllers import FlightInformation
+from Controllers import CalcDistance
+
+# Strip duplicate entries from the input file and re-write it
+StripErrors.start()
 
 # Determine the number of flights from each airport, including those with no flights
 NoOfFlightsFromAirports.start()
