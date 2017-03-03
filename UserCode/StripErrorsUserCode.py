cd50d0e5088a56f18e616823a2ddecca062a051b
George Bryant <george.s.bryant@gmail.com>
2017-03-03 18:09:38 +0000

    Log errors to console

--- UserCode/StripErrorsUserCode.py
+++ UserCode/StripErrorsUserCode.py
@@ -5,6 +5,8 @@ def mapDuplicates(self, inputLine):
     if stripErrors(inputLine):
         inputLine = inputLine.split(",")
         return KVPair(inputLine[0] + "," + inputLine[1], inputLine[2] + "," + inputLine[3] + "," + inputLine[4] + "," + inputLine[5])
+    else:
+        print(":: ERROR: Input line not propely formed, removed from dataset: " + inputLine)
 
 def mapSpelling(self, inputLine):
     words = inputLine.split(",")
@@ -13,6 +15,8 @@ def mapSpelling(self, inputLine):
     "iah", "clt", "mia", "muc", "kul", "fco", "ist"]
     if words[2] in airportCodes and words[3] in airportCodes:
          return KVPair(words[0] + "," + words[1], words[2] + "," + words[3] + "," + words[4] + "," + words[5])
+    else:
+        print(":: ERROR: Airport not found, removed from dataset: " + inputLine)
 
 def redWrite(self, kvPairs):
     return KVPair(kvPairs[0].key, kvPairs[0].value)
